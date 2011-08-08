124c1d91b87eaa7564b4767b5fa4e312c3088136
Greg Dobson <gregory.c.dobson@gmail.com>
2011-08-08 16:30:30 -0400

    implemented iterators, menu with different sized groups.  Space bar works for correct OS X settings

--- SP.rkt
+++ SP.rkt
@@ -1,97 +1,158 @@
 #lang racket
-(require 2htdp/batch-io racket/gui/base)
-(define names-filenames (read-csv-file "Class/Names.csv"))
-;(define end-of-list-sound 
+(require 2htdp/batch-io racket/gui/base framework)
+
+;declarations
+(struct person (name picture) #:transparent)
+
+
+;helper routines
+(define (sound-shuffle lst)
+  (play-sound "/System/Library/Sounds/Submarine.aiff" #t)
+  (shuffle lst))
+
 (define (read-picture triple) 
-  (list 
-   (first triple) 
-   (second triple) 
+  (person 
+   (string-append (first triple) " " (second triple)) 
    (read-bitmap (build-path "Class" (third triple)))))
-(define names-pics (map read-picture names-filenames))  
 
+(define (make-iterator lst func)
+  (define remaining lst)
+  (lambda ()
+    (define result (first remaining))
+    (set! remaining (rest remaining))
+    (when (empty? remaining)
+      (set! remaining (func lst)))
+    result))
 
-(define (divide-into-short-lists the-list n)
-  (if (<= (length the-list) n) 
+(define (divide-into-short-lists 
+         the-list 
+         num-lists)
+  (define total (length the-list))
+  (define chunk (floor (/ total num-lists)))
+  (if (= total chunk) 
       (list the-list)
-      (cons (take the-list n) (divide-into-short-lists (drop the-list n) n))))
-(define col-lists (divide-into-short-lists names-pics 3))
+      (cons (take the-list chunk) 
+            (divide-into-short-lists 
+             (drop the-list chunk) 
+             (sub1 num-lists)))))
+
+(define (pull-down-menu-pairs total )
+  (define (pdml k)
+    (define s (ceiling (/ total k)))
+    (if (<= (/ total k) 5) 
+        (list (list k s))
+        (cons (list k s) (pdml (ceiling (/ total (sub1 s)))))))
+  (pdml 1))
+
+(define (pull-down-menu-strings pairs)
+  (define (one-string pair)
+    (format "~a groups of size ~a" (first pair) (second pair)))
+  (map one-string pairs))
 
 
 
-
-
-
+;MAKE FRAME
 (define (make-frame col-lists)
-  (define pics-window 
-    (new frame% 
-         (label " Students ") 
-         (width 400)
-         (height 300)
-         (x 400)
-         (y 100)))
+  (define actual-frame%
+    (frame:standard-menus-mixin (frame:basic-mixin frame%)))
   
-  (define current-collection col-lists)
+  ; helper routines
   (define (process-next-group b e) 
     (send  pics-window  delete-child the-panel)
-    (set! current-collection (rest current-collection))
-    (when (empty? current-collection) 
-      (set! current-collection  col-lists))
-    (set! the-panel (make-panel (first current-collection))))  
-  (define top-panel 
-    (new horizontal-panel% (parent pics-window))) 
-  (define next-group-button  
-    (new button% 
-         (parent top-panel) 
-         (label "Next Group")
-         (callback process-next-group)))
-  #;(define group-size-box 
-    (new editor-canvas% 
-         (parent top-panel)	 
- 	 (editor (new text%))
-         (style '(no-hscroll no-vscroll))))
-  #;(send group-size-box allow-tab-exit #t)
-  (define group-size-box
-    (new text-field%
-         (label #f)
-         (parent top-panel)))
-  ;(define set-group-button)
+    (define the-group (iter-groups))
+    ;(displayln (format "current group =  ~a" (map person-name the-group)))
+    (set! the-panel (make-panel the-group)))
   
-  (define (make-panel one-list)
-    (define panel-shortcut%
-      (class vertical-panel%
-        (define/override (on-subwindow-char receiver event)
-          (displayln  (send event get-key-code))
-          (when (equal? (send event get-key-code) 'right) (send next-button command event))
-          #t)
-        (super-new)))
-    (define (get-picture triple) (third triple))
-    (define (get-name triple) (string-append (first triple) " " (second triple)))
-    (define pics-panel (new panel-shortcut% (parent pics-window)))
-    (define current-list one-list)
-    (define picture (new message% (parent pics-panel)
-                         (label (get-picture (first one-list)))))
-    (define name (new message% 
-                      (parent pics-panel) 
-                      (label (get-name (first one-list))) 
-                      (auto-resize #t)))  
+  (define (which-menu c e)
+    (define which (send c get-selection))
+    ;(displayln which)
+    (define nsets 
+      (first 
+       (first 
+        (drop menu-pairs which))))
+    ;(displayln (format " at which menu nsets= ~a" nsets))
+    (set! col-lists 
+          (divide-into-short-lists names-pics nsets))
+    (set! iter-groups (make-iterator col-lists identity)) 
+          ;(displayln (map person-name (first col-lists)))
+          (send  pics-window  delete-child the-panel)
+          (set! the-panel (make-panel (iter-groups))))
     
-    (define (process-next-person b e) 
-      (set! current-list (rest current-list))
-      (when (empty? current-list) 
-        (set! current-list (shuffle one-list))
-        (play-sound "/System/Library/Sounds/Submarine.aiff" #t))
-      (send picture set-label (get-picture (first current-list)))
-      (send name set-label (get-name (first current-list))))
-    (define next-button (new button% (parent pics-panel) (label "Next Person")
-                             (callback process-next-person)))
-    pics-panel)
+    ; MAKE PANEL
+    (define (make-panel one-list)
+      (define iter-people (make-iterator one-list sound-shuffle))
+      (define (process-next-person b e) 
+        (define one-person (iter-people))
+        (send picture set-label (person-picture one-person))
+        (send name set-label (person-name one-person)))
+      
+      ;setup the pane: picture, name, button for next person
+      (define pics-panel (new vertical-panel% (parent pics-window)))
+      (define picture (new message% (parent pics-panel)
+                           (label (make-bitmap 300 400))))
+      (define one-person (iter-people))
+      (send picture set-label (person-picture one-person))
+      
+      (define name (new message% 
+                        (parent pics-panel) 
+                        (label (person-name one-person)) 
+                        (auto-resize #t)))  
+      (define next-button (new button% (parent pics-panel) (label "Next Person")
+                               (callback process-next-person)))
+      pics-panel)
+    
+    
+    ;doing stuff
+    (define pics-frame 
+      (new actual-frame% 
+           (label " Students ") 
+           (width 400)
+           (height 300)
+           (x 400)
+           (y 100)))
+    (define pics-window (send pics-frame get-area-container))
+    
+    ;(displayln (format "starting frame #of collections= ~a" (length col-lists)))
+    
+    ; Setup the top panel on the frame:  next group button and pull down menu
+    (define top-panel 
+      (new horizontal-panel% (parent pics-window)))
+    
+    (define next-group-button  
+      (new button% 
+           (parent top-panel) 
+           (label "Next Group")
+           (callback process-next-group)))
+    
+    ; create the pull down menu for "how many groups of what size"
+    (new choice% 
+         (label #f)
+         (choices menu-strings )
+         (parent top-panel)
+         (callback which-menu))
+    
+    
+    (define the-panel (make-panel (iter-groups)))   
+    (send pics-frame show #t)) 
   
-  (define the-panel (make-panel (first col-lists)))   
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;read in names, pictures, and create initial list-of-groups
+  (define names-filenames (read-csv-file "Class/Names.csv"))
+  (define names-pics (map read-picture names-filenames))  
+  (define list-of-groups (divide-into-short-lists names-pics 3))
+  (define iter-groups (make-iterator list-of-groups identity))
   
-  (send pics-window show #t))
-
-(make-frame col-lists)
-
-
-
-
+  ;create the pull down menu 
+  (define menu-pairs 
+    (pull-down-menu-pairs
+     (length names-pics)))
+  (define menu-strings
+    (pull-down-menu-strings menu-pairs))
+  
+  ;do it
+  (make-frame list-of-groups)
+  
+  
+  
+  
+  
